dfed24ae03a7ec4e04a99c95e02dd42c457238c9
Sergei <sy.kotov@gmail.com>
2024-08-05 17:55:25 +0300

    char info features added

--- src/components/charInfo/CharInfoView/CharInfoView.tsx
+++ src/components/charInfo/CharInfoView/CharInfoView.tsx
@@ -2,34 +2,38 @@ import React, { FC } from "react";
 import { CharacterInfo } from "../../../types/types";
 
 const CharInfoView: FC<{char: CharacterInfo}> = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki, comics} = char;
+    const imgClasses = (thumbnail.indexOf('image_not_available') === -1) ? "" : "char__img_nf";
     return (
     <>
         <div className="char__basics">
-            <img src={char.thumbnail} alt={char.name}/>
+            <img className={imgClasses} src={thumbnail} alt={name}/>
             <div>
-                <div className="char__info-name">{char.name}</div>
+                <div className="char__info-name">{name}</div>
                 <div className="char__btns">
-                    <a href={char.homepage} className="button button__main">
+                    <a href={homepage} className="button button__main">
                         <div className="inner">homepage</div>
                     </a>
-                    <a href={char.wiki} className="button button__secondary">
+                    <a href={wiki} className="button button__secondary">
                         <div className="inner">Wiki</div>
                     </a>
                 </div>
             </div>
         </div>
-        <div className="char__descr">{char.description}</div>
+        <div className="char__descr">{description}</div>
         <div className="char__comics">Comics:</div>
-        {char.comics && char.comics.length > 0 && 
+        {comics && comics.length > 0 ? 
         <ul className="char__comics-list">
             {
-                char.comics.map((item, index) => (
+                comics.slice(0, 10).map((item, index) => (
                     <li key={index} className="char__comics-item">
                         {item.name}
                     </li>
                 ))
             }
-        </ul>}
+        </ul> :
+        <div>There are no comics with this character</div>
+        }
     </>
     )
 }
